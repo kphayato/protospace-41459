07168cb616d7f8255c45b63dd9e13caa9eb3f7ee
kphayato <hayato.k.0608@gmail.com>
2024-11-22 00:27:10 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_21_133010) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
